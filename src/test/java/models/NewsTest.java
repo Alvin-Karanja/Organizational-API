01c9a9bee2bfa4d75a7ba95403c7d064d6ddcae0
Alvin-Karanja <alvin2@protonmail.com>
2019-10-27 16:46:38 +0300

    Added NewsTest

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,4 +1,47 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class NewsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void NewsInstantiatesCorrectly() throws Exception{
+        News news = setupNews();
+        assertTrue(news instanceof News);
+    }
+
+    @Test
+    public void getNewsNameReturnsCorrectName() throws Exception {
+        News testNews = setupNews();
+        assertEquals("Rescues", testNews.getNews_name());
+    }
+    @Test
+    public void setNewsId() throws Exception{
+        News news = setupNews();
+        news.setNews_id(1);
+        assertNotEquals(3,news.getNews_id());
+    }
+    @Test
+    public void NewsInstantiatesCorrectlyWithCorrect_Values() throws Exception{
+        News news = setupNews();
+        assertEquals("Rescues",news.getNews_name());
+        assertEquals("Fire Department Rescues six",news.getNews_content());
+        assertEquals(1,news.getDpt_id());
+    }
+
+    public News setupNews() {
+        return new News(1, "Rescues", "Fire Department Rescues six");
+    }
 }
