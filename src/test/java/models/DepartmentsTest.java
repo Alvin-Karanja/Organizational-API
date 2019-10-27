261f64ee9dc40132b7ca68dcd9e4b83b26c55c15
Alvin-Karanja <alvin2@protonmail.com>
2019-10-27 15:08:01 +0300

    Added test to get Dpt Name correctly

--- src/test/java/models/DepartmentsTest.java
+++ src/test/java/models/DepartmentsTest.java
@@ -2,6 +2,9 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class DepartmentsTest {
     @Before
@@ -12,5 +15,11 @@ public class DepartmentsTest {
     @After
     public void tearDown() throws Exception {
 
+    }
+
+    @Test
+    public void getDepartmentNameReturnsCorrectName() throws Exception {
+        Departments testDepartments = setUpDepartments();
+        assertEquals("Fire Department", testDepartments.getDpt_name());
     }
 }
