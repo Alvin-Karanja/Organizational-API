578d5a4cc724f26d28bbbfc5ab361f599f82af46
Alvin-Karanja <alvin2@protonmail.com>
2019-10-27 15:39:13 +0300

    Added methods

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -6,4 +6,13 @@ public class Departments {
 
     public String getDpt_description() {
     }
+
+    public void setDpt_empNo(int i) {
+    }
+
+    public long getDpt_empNo() {
+    }
+
+    public void setDpt_name(String fire_department) {
+    }
 }
