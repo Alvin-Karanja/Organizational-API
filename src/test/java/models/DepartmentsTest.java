c8ddf691c220242ddd84993f1a0cca46bb4b0981
Alvin-Karanja <alvin2@protonmail.com>
2019-10-27 15:03:22 +0300

    Added setup and teardown methods

--- src/test/java/models/DepartmentsTest.java
+++ src/test/java/models/DepartmentsTest.java
@@ -1,4 +1,16 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
+
 public class DepartmentsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
 }
