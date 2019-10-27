c5e2370d32579404f4d1d106ed41845c9fa353fd
Alvin-Karanja <alvin2@protonmail.com>
2019-10-27 16:21:54 +0300

    Added dpt_description getter

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,10 +1,26 @@
 package models;
 
+import java.util.Objects;
+
 public class Departments {
+    private String dpt_name;
+    private String dpt_description;
+    private int dpt_empNo;
+    private int id;
+
+
+    public Departments(String dpt_name, String dpt_description, int dpt_empNo) {
+        this.dpt_name = dpt_name;
+        this.dpt_description = dpt_description;
+        this.dpt_empNo = dpt_empNo;
+
+    }
+
     public String getDpt_name() {
     }
 
     public String getDpt_description() {
+        return dpt_description;
     }
 
     public void setDpt_empNo(int i) {
